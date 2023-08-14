2045bd0677681d408de44253f45bce25a7e52fc5
Gunjanraj321 <118360371+Gunjanraj321@users.noreply.github.com>
2023-08-14 21:35:47 +0530

    changes

--- dom.js
+++ dom.js
@@ -1,5 +1,31 @@
-// console.log(document)
-// console.dir(document)
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title=123;
+// console.log(document.head);
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.getElementById('header-title'));
+//  var headerTitle=document.getElementById('header-title');
+//  var header=document.getElementById('main-header');
 
-console.log(document.URL);
-console.log
+// //  console.log(headerTitle);
+// // // headerTitle.textContent='Hello';
+//  console.log(headerTitle.innerText);
+//  headerTitle.textContent='hello world';
+// // headerTitle.innerText='goodbye';
+// // headerTitle.innerHTML='<h1>Helllo</h1>';
+// //headerTitle.style.borderBottom="solid 3px #000";
+// header.style.borderBottom='Solid 3px #000';
+// console.log(document.getElementById);
+var items=document.getElementsByClassName('list-group-item');
+ console.log(items);
+ console.log(items[1]);
+ items[1].textContent='Hello 2';
+ items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+
