27022840ac86caef66f79a8d69a16b0160c7cbcf
Gunjanraj321 <118360371+Gunjanraj321@users.noreply.github.com>
2023-08-14 21:46:07 +0530

    font _bold&items3

--- dom.js
+++ dom.js
@@ -28,7 +28,11 @@ var items=document.getElementsByClassName('list-group-item');
  //items[1].textContent='Hello 2';
  //items[1].style.fontWeight='bold';
  //items[1].style.backgroundColor='yellow';
- for(var i=0;i<items.length;i++){
-    items[i].style.backgroundColor='#f4f4f4';
- }
+//  for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='#f4f4f4';
+//  }
+items[3].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+        items[i].style.fontWeight='bold';
+     }
 
