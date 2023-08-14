46dde5c17475139b6c1fcb9913f487962f084f65
Gunjanraj321 <118360371+Gunjanraj321@users.noreply.github.com>
2023-08-14 22:21:18 +0530

    new change

--- dom.js
+++ dom.js
@@ -22,17 +22,24 @@
 // //headerTitle.style.borderBottom="solid 3px #000";
 // header.style.borderBottom='Solid 3px #000';
 // console.log(document.getElementById);
-var items=document.getElementsByClassName('list-group-item');
- console.log(items);
- console.log(items[1]);
- //items[1].textContent='Hello 2';
- //items[1].style.fontWeight='bold';
- //items[1].style.backgroundColor='yellow';
-//  for(var i=0;i<items.length;i++){
-//     items[i].style.backgroundColor='#f4f4f4';
-//  }
-items[3].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-        items[i].style.fontWeight='bold';
-     }
+// var items=document.getElementsByClassName('list-group-item');
+//  console.log(items);
+//  console.log(items[1]);
+//  //items[1].textContent='Hello 2';
+//  //items[1].style.fontWeight='bold';
+//  //items[1].style.backgroundColor='yellow';
+// //  for(var i=0;i<items.length;i++){
+// //     items[i].style.backgroundColor='#f4f4f4';
+// //  }
+// items[3].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//         items[i].style.fontWeight='bold';
+//      } 
+var li=document.getElementsByTagName('li');
+// li[1].textContent='Hello';
+// for(var i=0;i<li.length;i++){
+//         li[i].style.backgroundColor='#f4f4f5';
+//      }
+console.log(document.getElementsByTagName(li[4]));
+
 
