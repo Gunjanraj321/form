bfd27d60fba9bc62a9c40bbdce9acbb98d8e90a1
Gunjanraj321 <118360371+Gunjanraj321@users.noreply.github.com>
2023-08-14 21:42:46 +0530

    change

--- dom.js
+++ dom.js
@@ -25,7 +25,10 @@
 var items=document.getElementsByClassName('list-group-item');
  console.log(items);
  console.log(items[1]);
- items[1].textContent='Hello 2';
- items[1].style.fontWeight='bold';
- items[1].style.backgroundColor='yellow';
+ //items[1].textContent='Hello 2';
+ //items[1].style.fontWeight='bold';
+ //items[1].style.backgroundColor='yellow';
+ for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+ }
 
